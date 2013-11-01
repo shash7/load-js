d7e4736a0f0473e0d0696697cb7fd1d9dd2a210d
shash7 <shashwat.amin@yahoo.com>
2013-11-01 18:19:03 +0530

    More sturdy api design.
    Added minified file which needs to be tested.

--- load.js
+++ load.js
@@ -1,52 +1,52 @@
 // load.js - load js/css resources only once with javascript calls
-// version 0.2
+// version 0.5
 
-;(function(window) {
+;(function(window, document) {
 	
 	"use strict";
 	
-	var loader = {
-		sources : {},
-		load_js : function(src) {
-			if(src in loader.sources) {
+	window.load = function() {
+		var sources = {};    // Cache
+		var js = function(src) {
+			if(src in sources) {
 				return;
 			}
 			document.write("<script type='text/javascript' src='" + src + "'><\/script>");
-			loader.sources[src] = true;
-		},
+			sources[src] = true;
+		};
 		
 		
-		load_css :function(src) {
-			if(src in loader.sources) {
+		var css = function(src) {
+			if(src in sources) {
 				return;
 			}
 			document.write("<link type='text/css' rel='stylesheet' href='" + src + "' />");
-			loader.sources[src] = true;
-		},
+			sources[src] = true;
+		};
 		
 		
-		load_jquery : function(version) {
+		var jquery = function(version) {
 			version = version || 1;
 			if(!window.jQuery) {
-				loader.load_js('http://ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.js');
+				js('http://ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.js');
 			}
-		},
+		};
 		
 		
-		load_jquery_ui : function(version) {
-			loader.load_jquery();
+		var jqueryUi = function(version) {
+			jquery();
 			version = version || '1.10.0';
 			if(!(window.jQuery && jQuery.ui)) {
-				loader.load_css('http://code.jquery.com/ui/' + version + '/themes/base/jquery-ui.css');
-				loader.load_js('http://code.jquery.com/ui/'  + version + '/jquery-ui.js');
+				css('http://code.jquery.com/ui/' + version + '/themes/base/jquery-ui.css');
+				js('http://code.jquery.com/ui/'  + version + '/jquery-ui.js');
 			}
-		}
+		};
+		
+		return {
+			js       : js,
+			css      : css,
+			jquery   : jquery,
+			jqueryUi : jqueryUi
+		};
 	};
-	
-	
-	/* For maintaining backward compability */
-	window.load_js        = loader.load_js;
-	window.load_css       = loader.load_css;
-	window.load_jquery    = loader.load_jquery;
-	window.load_jquery_ui = loader.load_jquery_ui;
-})(window);
+})(window, document);
